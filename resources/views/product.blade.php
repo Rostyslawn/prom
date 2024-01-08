d146eda5da2ae1a7f5047ef0b62ebd57d1104755
Rostyslaw <75090630+Rostyslawn@users.noreply.github.com>
2024-01-09 01:00:02 +0200

    product page update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,13 +1,10 @@
 @extends("app.my-layout")
-@section("scripts")
-
-@endsection
 @section("styles")
     <link rel="stylesheet" href="{{asset('css/product.css')}}">
 @endsection
 @section("title", $product_data->name)
 @section("content")
-{{--    {{$product_data}}--}}
+    {{--    {{$product_data}}--}}
     <div class="category-nav block">
         <div class="item catalog"><a href="#">Каталог товарiв</a></div>
         <div class="item arrow"><img alt=">" src="{{asset("imgs/arrow.png")}}"></div>
@@ -16,40 +13,65 @@
         <div class="item catalog-name"><a href="#">Шампунi</a></div>
     </div>
     <div class="main block">
-        <div class="container main-img">
-            <img alt="{{$product_data->name}}" src="{{$product_data->img}}">
+        <div class="container characteristics">
+            <div class="item characteristics-img">
+                <img alt="{{$product_data->name}}" src="{{$product_data->img}}">
+            </div>
+            <div class="item-margin item about-product">
+                <h2>Характеристики та опис</h2>
+            </div>
         </div>
-        <div class="container product-info">
-            <span class="head">
-                <div class="product_name">{{$product_data->name}}, {{$product_data->amount}}шт.</div>
-                <button class="like"><img alt="like" src="{{asset('imgs/purpleHeart.png')}}"></button>
-            </span>
-            <div class="delivery-status">
-                @if($product_data->amount > 0)
-                    <div class="ready">Товар в наявностi</div>
+        <div class="container">
+            <div class="item product-info">
+                <span class="head">
+                    <div class="product_name">{{$product_data->name}}, {{$product_data->amount}}шт.</div>
+                    <button class="like"><img alt="like" src="{{asset('imgs/purpleHeart.png')}}"></button>
+                </span>
+                <div class="delivery-status">
+                    @if($product_data->amount > 0)
+                        <div class="ready">Товар в наявностi</div>
+                    @else
+                        <div class="not-ready">Товар закiнчився</div>
+                    @endif
+                </div>
+                @if($product_data->sale)
+                    <div class="sale">{{$product_data->sale}} ₴</div>
+                    <div class="old-price">{{$product_data->price}} ₴</div>
                 @else
-                    <div class="not-ready">Товар закiнчився</div>
+                    <div class="price">{{$product_data->price}} ₴</div>
                 @endif
+                <div class="buttons">
+                    <button class="buy-product">
+                        <img src="{{asset('imgs/whitecart.png')}}">
+                        <span>Купити</span>
+                    </button>
+                    <button class="chat-with-seller">
+                        <img src="{{asset('imgs/chat.png')}}">
+                        <span>Чат з продавцем</span>
+                    </button>
+                </div>
+                <div class="seller">
+                    <a href="#">Продавець {{$product_data->seller}} <img src="{{asset('imgs/arrow.png')}}"></a>
+                </div>
             </div>
-            @if($product_data->sale)
-                <div class="sale">{{$product_data->sale}} ₴</div>
-                <div class="old-price">{{$product_data->price}} ₴</div>
-            @else
-                <div class="price">{{$product_data->price}} ₴</div>
-            @endif
-            <div class="buttons">
-                <button class="buy-product">
-                    <img src="{{asset('imgs/whitecart.png')}}">
-                    <span>Купити</span>
-                </button>
-                <button class="chat-with-seller">
-                    <img src="{{asset('imgs/chat.png')}}">
-                    <span>Чат з продавцем</span>
-                </button>
-            </div>
-            <div class="seller">
-                <a href="#">Продавець {{$product_data->seller}}</a>
+            <a href="#" class="item-margin item cards">
+                <span>Сплачуй безпечно карткою</span>
+                <img src="{{asset('imgs/cards.png')}}">
+            </a>
+            <div class="item-margin item delivery">
+                <div class="delivery-service">
+                    <img src="{{asset('imgs/novaposhta.jpg')}}">
+                    <span>Нова пошта</span>
+                </div>
+                <button class="see-all">Дивитись все</button>
             </div>
         </div>
     </div>
+    <div class="modals">
+        <div onclick="close_modals()" class="modal-bg"></div>
+
+    </div>
+@endsection
+@section('scripts')
+    <script src="{{asset('js/modalsProduct.js')}}"></script>
 @endsection
