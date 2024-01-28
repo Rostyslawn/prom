0900ebf6809de876497aa64d3a971f424f21840b
Rostyslaw <75090630+Rostyslawn@users.noreply.github.com>
2024-01-28 15:28:53 +0200

    scss

--- resources/views/app/my-layout.blade.php
+++ resources/views/app/my-layout.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{asset('imgs/icon.jpg')}}">
-    <link rel="stylesheet" href="{{asset('css/nav.css')}}">
-    <link rel="stylesheet" href="{{asset('css/modals.css')}}">
-    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+{{--    <link rel="stylesheet" href="{{asset('css/nav.css')}}">--}}
+{{--    <link rel="stylesheet" href="{{asset('css/modals.css')}}">--}}
+{{--    <link rel="stylesheet" href="{{asset('css/footer.css')}}">--}}
+    @vite("resources/scss/imports.scss")
     @yield("styles")
     <title>@yield("title", "Prom")</title>
 </head>
